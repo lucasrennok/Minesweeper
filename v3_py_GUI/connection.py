2419d890a3060f632f7e17560a7ac1f00a97179a
Lucas Rennó Kallás <lucasrk@unifei.edu.br>
2020-07-23 15:10:32 -0300

    create and connection

--- v3_py_GUI/connection.py
+++ v3_py_GUI/connection.py
@@ -6,27 +6,32 @@ buffer = ""
 host_server = ""
 port = 1500
 
-def receive_data():
-    while(1):#enquanto jogo não acabar
-        mesage = ""
-        receive,client = server_socket.recvfrom(2048)
-        #tem que pegar esses dados e enviar pra matriz
-        mesage = receive.decode() 
-        if(mesage==""):
-            server_socket.sendto(buffer.encode(), client)
+def receiveMesage():
+    global client_socket
+    global host_server
+    global port
+    buffer_history = ""
+    req = ""
+    timer = 0
+    while(1): #enquanto jogo continuar rodando
+        if(timer%5==0):
+            timer=0
+            client_socket.sendto(req.encode(), (host_server,port))
+            buffer,server = client_socket.recvfrom(2048)    #recebe dados no buffer pra atualizar o campo minado
+            if(buffer.decode()!=buffer_history):
+                pass    # envia dados pra matriz
+            buffer_history = buffer.decode()
+        time.sleep(1)
+        timer+=1
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-local = ("",port)
-server_socket.bind(local)
-
-t1 = threading.Thread(target=receive_data)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+t1 = threading.Thread(target=receiveMesage)
 t1.start()
 
-while(1):#enquanto jogo não acabar
-    if(buffer=="nothing"):
-        pass
+while(1): #enquanto jogo está rodando
+    mesage = input("")    #recebe entrada do botão, envia pro server
+    client_socket.sendto(mesage.encode(),(host_server,port))
 
-print("\nClosing in 3 secs...")
-time.sleep(3)
+print("Closing Chat")
 
-server_socket.close()
+client_socket.close()
---
+++ v3_py_GUI/create_game.py
@@ -0,0 +1,31 @@
+import socket
+import threading
+import time
+
+buffer = ""
+host_server = ""
+port = 1500
+
+def receive_data():
+    mesage = ""
+    while(1):   #enquanto jogo não acabar
+        receive,client = server_socket.recvfrom(2048)
+        mesage = receive.decode() 
+        if(mesage==""):
+            server_socket.sendto(buffer.encode(), client)
+        else:
+            pass    #tem que pegar esses dados e enviar pra matriz
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind(("", port))
+
+t1 = threading.Thread(target=receive_data)
+t1.start()
+
+while(1):   #enquanto jogo não acabar
+    pass # le botao clicado e coloca no buffer e na matriz
+
+print("\nClosing in 5 secs...")
+time.sleep(5)
+
+server_socket.close()
